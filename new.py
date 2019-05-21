0dda96c25eaa98be6ddd594079f10b108352944d
hardertobreathe <hardertobreathe@naver.com>
2019-05-21 17:47:39 +0900

    pw

--- new.py
+++ new.py
@@ -8,7 +8,7 @@ driver.implicitly_wait(3)
 driver.get('https://nid.naver.com/nidlogin.login')
 
 driver.find_element_by_name('id').send_keys('hardertobreathe')
-driver.find_element_by_name('pw').send_keys('thislove12')
+driver.find_element_by_name('pw').send_keys('password')
 driver.find_element_by_xpath('//*[@id="frmNIDLogin"]/fieldset/input').click()
 
 driver.get('https://order.pay.naver.com/home')

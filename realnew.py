12bbec69e1682334942c4c4f889d841d55da84bd
hardertobreathe <hardertobreathe@naver.com>
2019-05-21 18:03:36 +0900

    mmm

--- realnew.py
+++ realnew.py
@@ -1,3 +1,9 @@
 from selenium import webdriver
 
-browser = webdriver.Chrome()
+path = "C:\\Users\\오정인\\Desktop\\chromedriver_win32\\chromedriver"
+driver = webdriver.Chrome(path)
+
+driver.get('https://www.naver.com')
+search_box = driver.find_element_by_name('query')
+search_box.send_keys("빅뱅")
+search_box.submit()
